e91847dc665685afe53ead9224a66130be481fc1
Eoin Hanan <14176459@studentmail.ul.ie>
2017-10-03 14:23:16 +0100

    Placeholders for testing Input

---
+++ src/Character/Observer.java
@@ -0,0 +1,8 @@
+package Character;
+
+/**
+ * Created by EoinH on 02/10/2017.
+ */
+public interface Observer {
+    public void update(String Command);
+}
---
+++ src/Character/Proxy.java
@@ -0,0 +1,16 @@
+package Character;
+
+/**
+ * Created by EoinH on 03/10/2017.
+ */
+public class Proxy implements Observer {
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void update(String Command) {
+        System.out.println("Incoming command is " + Command);
+    }
+}
