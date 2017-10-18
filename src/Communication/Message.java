2836d2b5d5700f90c84d01e54758228ad3657463
Patrick Cumiskey <14167387@studentmail.ul.ie>
2017-10-18 13:50:56 +0100

    Message Action addition

--- src/Communication/Message.java
+++ src/Communication/Message.java
@@ -7,11 +7,13 @@ public class Message {
     private String destination;
     private String source;
     private String content;
+    private String action;
 
-    public Message(String destination, String source, String content){
+    public Message(String destination, String source, String content, String action){
         this.destination = destination;
         this.source = source;
         this.content = content;
+        this.action = action;
     }
 
     public void setDestination(String destination){
@@ -23,6 +25,9 @@ public class Message {
     public void setContent(String content){
         this.content= content;
     }
+    public void setAction(String action){
+        this.action= action;
+    }
 
     public String getDestination(){
         return destination;
@@ -32,5 +37,8 @@ public class Message {
     }
     public String getContent(){
         return content;
+    }
+    public String getAction(){
+        return action;
     }
 }
