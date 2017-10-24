e60beec5bbdf994918c61b6681c9e505790dd749
Patrick Cumiskey <14167387@studentmail.ul.ie>
2017-10-24 11:29:19 +0100

    takeinput  and main

---
+++ src/Input/TakeInput.java
@@ -0,0 +1,16 @@
+package Output;
+
+import java.util.Scanner;
+
+public class TakeInput {
+
+    public String userinput (){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Command");
+        String command = sc.nextLine();
+
+        return command;
+    }
+}
+
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
+import java.util.Scanner;
+import Output.TakeInput;
 /**
  * Created by EoinH on 28/09/2017.
  */
 public class Main {
     public static void main(String [] args){
+        TakeInput input = new TakeInput();
+        String usertext = input.userinput();
+        System.out.println(usertext);
 
     }
 }
